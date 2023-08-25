11933f31b4481206efa42e2f907293f55f004166
memlocator <apakokos@gmail.com>
2023-08-26 01:33:56 +0200

    fixed errors, moved rail generating script to rail node

--- Extrude.gd
+++ Extrude.gd
@@ -1,6 +1,8 @@
 @tool
-extends MeshInstance3D
+extends Path3D
 var sleepers = []
+var mesh = MeshInstance3D.new()
+
 
 @export_group("Rail Parameters")
 @export_range(0.05, 1, 0.1) var segment_dist: float = 0.1:
@@ -24,7 +26,7 @@ var sleepers = []
 		generate_rails()
 		
 @export var Regen: bool = false:
-	set(button):
+	set(f):
 		generate_rails()
 		generate_sleepers()
 	
@@ -38,7 +40,7 @@ func offset_vertices(vertices, vector):
 	return vertices
 # Called when the node enters the scene tree for the first time.
 
-var cross_section_points = PackedVector3Array([Vector3(-0.7923468947410583, -0.0031647791620343924, 0), Vector3(-0.7923470139503479, -0.07024983316659927, 0), Vector3(-0.39617350697517395, -0.07024992257356644, 0), Vector3(-3.756487444240078e-14, -0.0702500119805336, 0), Vector3(0.39617350697517395, -0.07024992257356644, 0), Vector3(0.7923470139503479, -0.07024983316659927, 0), Vector3(0.7923470139503479, -0.003164831316098571, 0), Vector3(0.7923470139503479, 0.06392016261816025, 0), Vector3(0.7343460321426392, 0.07994666695594788, 0), Vector3(0.6763449907302856, 0.09597316384315491, 0), Vector3(0.3989979922771454, 0.11886816471815109, 0), Vector3(0.12165100127458572, 0.14176316559314728, 0), Vector3(0.12058250606060028, 0.5042691826820374, 0), Vector3(0.11951400339603424, 0.8667751550674438, 0), Vector3(0.14515650272369385, 0.8933336734771729, 0), Vector3(0.17079900205135345, 0.9198921918869019, 0), Vector3(0.29534849524497986, 0.958355724811554, 0), Vector3(0.41989800333976746, 0.9968191981315613, 0), Vector3(0.4391300082206726, 1.0096397399902344, 0), Vector3(0.45836201310157776, 1.0224602222442627, 0), Vector3(0.4583619236946106, 1.1224602460861206, 0), Vector3(0.4583618640899658, 1.2224602699279785, 0), Vector3(0.4363824129104614, 1.2362003326416016, 0), Vector3(0.4144029915332794, 1.249940276145935, 0), Vector3(0.3731920123100281, 1.257265329360962, 0), Vector3(0.33198100328445435, 1.2645902633666992, 0), Vector3(0.16599050164222717, 1.2645902633666992, 0), Vector3(-3.756487444240078e-14, 1.2645902633666992, 0), Vector3(-0.1659904420375824, 1.264589548110962, 0), Vector3(-0.3319808840751648, 1.2645889520645142, 0), Vector3(-0.37319207191467285, 1.2572624683380127, 0), Vector3(-0.4144032597541809, 1.2499361038208008, 0), Vector3(-0.43638256192207336, 1.2361990213394165, 0), Vector3(-0.4583618640899658, 1.2224619388580322, 0), Vector3(-0.4583618640899658, 1.1224619150161743, 0), Vector3(-0.4583618640899658, 1.0224618911743164, 0), Vector3(-0.4391299784183502, 1.0096406936645508, 0), Vector3(-0.4198980927467346, 0.9968193173408508, 0), Vector3(-0.29534873366355896, 0.9583555459976196, 0), Vector3(-0.1707993894815445, 0.9198917746543884, 0), Vector3(-0.1451568752527237, 0.8933334946632385, 0), Vector3(-0.11951436847448349, 0.8667751550674438, 0), Vector3(-0.12058280408382416, 0.5042694211006165, 0), Vector3(-0.12165124714374542, 0.1417636126279831, 0), Vector3(-0.39899808168411255, 0.11886851489543915, 0), Vector3(-0.6763449311256409, 0.0959734097123146, 0), Vector3(-0.7343458533287048, 0.0799468383193016, 0), Vector3(-0.7923467755317688, 0.0639202669262886, 0)])
+var cross_section_points = PackedVector3Array([Vector3(-0.7923467755317688, 0.0639202669262886, 0), Vector3(-0.7923470139503479, -0.07024983316659927, 0), Vector3(0.0, -0.0702500119805336, 0), Vector3(0.7923470139503479, -0.07024983316659927, 0), Vector3(0.7923467755317688, 0.0639202669262886, 0), Vector3(0.12165124714374542, 0.1417636126279831, 0), Vector3(0.11951436847448349, 0.8667751550674438, 0), Vector3(0.4583618640899658, 1.0224618911743164, 0), Vector3(0.4583618640899658, 1.2224619388580322, 0), Vector3(0.3319808840751648, 1.2645889520645142, 0), Vector3(0.0, 1.2645902633666992, 0), Vector3(-0.3319808840751648, 1.2645889520645142, 0), Vector3(-0.4583618640899658, 1.2224619388580322, 0), Vector3(-0.4583618640899658, 1.0224618911743164, 0), Vector3(-0.11951436847448349, 0.8667751550674438, 0), Vector3(-0.12165124714374542, 0.1417636126279831, 0)])
 var cross_section_points_len = len(cross_section_points)
 
 func new_segment(transform, offset_vec = Vector3()):
@@ -63,21 +65,20 @@ func place_sleeper(transform):
 
 func generate_sleepers():
 	clear_sleepers()
-	
-	var path = $"../Rail" as Path3D
 
-	var curve_length = path.curve.get_baked_length()
+	var curve_length = curve.get_baked_length()
 	for i in range(0, curve_length*100, sleeper_dist*100):
-		var transform = path.curve.sample_baked_with_rotation(float(i)/100, true)
+		var transform = curve.sample_baked_with_rotation(float(i)/100, true)
 		place_sleeper(transform)
 	
 		
 
 func generate_rails():
-	mesh = ArrayMesh.new()
+	mesh.mesh = ArrayMesh.new()
 	generate_rail(offset)
 	generate_rail(-offset)
 	generate_sleepers()
+
 
 func place_cross_section(cross_sections, transform, rail_offset):
 	var curr_segment = new_segment(transform)
@@ -93,17 +94,15 @@ func generate_rail(rail_offset):
 	var cross_sections_count = 0
 	var indices = PackedInt32Array()
 
-	var path = $"../Rail" as Path3D
-
-	var curve_length = path.curve.get_baked_length()
+	var curve_length = curve.get_baked_length()
 	var segment_target = curve_length/segment_dist
 	for i in range(0, curve_length*100, segment_dist*100):
-		var transform = path.curve.sample_baked_with_rotation(float(i)/100, true)
+		var transform = curve.sample_baked_with_rotation(float(i)/100, true)
 		place_cross_section(cross_sections, transform, rail_offset)
 		cross_sections_count += 1
 	
 	#need to guarantee we reach the end of the curve
-	var transform = path.curve.sample_baked_with_rotation(curve_length, true)
+	var transform = curve.sample_baked_with_rotation(curve_length, true)
 	place_cross_section(cross_sections, transform, rail_offset)
 	cross_sections_count += 1
 	##################################################
@@ -161,11 +160,11 @@ func generate_rail(rail_offset):
 	surface_array[Mesh.ARRAY_VERTEX] = verts
 	surface_array[Mesh.ARRAY_INDEX] = indices
 	surface_array[Mesh.ARRAY_NORMAL] = normals
-	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
+	mesh.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
 
 func _ready():
-	var path = $"../Rail" as Path3D
 	generate_rails()
+	add_child(mesh)
 	
 func _process(delta):
 	pass
